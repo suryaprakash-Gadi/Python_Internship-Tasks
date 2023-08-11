967e22321799e22716ec74f4bb2eaff5599c7a9a
surya prakash Gadi <105710300+suryaprakash-Gadi@users.noreply.github.com>
2023-08-11 21:20:37 +0530

    Create String Reversal.py

---
+++ String Reversal.py
@@ -0,0 +1,10 @@
+print(" Task_1 -String Reversal")
+print("-----------------------")
+
+def string_reversal(input_string):
+    revers=(input_string[::-1]) 
+    return revers   
+
+input_string =input("Enter a string to revers::") 
+print(string_reversal(input_string))
+
